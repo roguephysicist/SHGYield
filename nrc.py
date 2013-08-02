3b058b5c88239d9a606bb4a20a45033a04616276
roguephysicist <sean.martin.anderson@gmail.com>
2013-08-02 13:15:09 -0500

    general cleanup of functions and variables, updated todo

--- nrc.py
+++ nrc.py
@@ -13,8 +13,6 @@ exit = "s" # 's' or 'p'
 # Angles
 theta_deg = 65 # angle of incidence in degrees
 phi_deg = 30 # azimuthal angle
-# Output
-out_file_path = file_path + "R" + entry + exit + "_" + str(kpts) + "_" + str(ecut)
 
 ## Housekeeping
 n_0 = 6.02214129e+23 # electronic density - for testing purposes
@@ -24,78 +22,82 @@ phi = math.radians(phi_deg)
 ## Functions
 def nonlinear_reflection_coefficient(polar_in, polar_out):
 	nrc = []
-	omeg = [row[0] for row in values("linear")]
-	epsi = [row[1] for row in values("linear")]
-	chi_zzz = [row[1] for row in values("zzz")]
-	chi_zxx = [row[1] for row in values("zxx")]
-	chi_xxz = [row[1] for row in values("xxz")]
-	chi_xxx = [row[1] for row in values("xxx")]
-	for omega, epsilon, zzz, zxx, xxz, xxx in izip(omeg, epsi, chi_zzz, chi_zxx, chi_xxz, chi_xxx):
-		R = rif_constants(float(omega)) * math.fabs(fresnel(polar_out, "vs", float(omega), float(epsilon)) * fresnel(polar_out, "sb", float(omega), float(epsilon)) * ((fresnel(polar_in, "vs", float(omega), float(epsilon)) * fresnel(polar_in, "sb", float(omega), float(epsilon))) ** 2) * r_factors(polar_in, polar_out, float(zzz), float(zxx), float(xxz), float(xxx), float(omega), float(epsilon))) ** 2
-		nrc.append(R)
-	omeg.extend(nrc)
-	return omeg
+	energy = [row[0] for row in values("epsilon")]
+	epsilon = [row[1] for row in values("epsilon")]
+	zzz = [row[0] for row in values("zzz")]
+	zxx = [row[0] for row in values("zxx")]
+	xxz = [row[0] for row in values("xxz")]
+	xxx = [row[0] for row in values("xxx")]
+	for energy, epsilon, zzz, zxx, xxz, xxx in izip(energy, epsilon, zzz, zxx, xxz, xxx):
+		R = rif_constants(float(energy)) * math.fabs(fresnel(polar_out, "vs", float(energy), float(epsilon)) * fresnel(polar_out, "sb", float(energy), float(epsilon)) * ((fresnel(polar_in, "vs", float(energy), float(epsilon)) * fresnel(polar_in, "sb", float(energy), float(epsilon))) ** 2) * r_factors(polar_in, polar_out, float(zzz), float(zxx), float(xxz), float(xxx), float(energy), float(epsilon))) ** 2
+		nrc.append(str(R))
+	return nrc
 
 def values(component):
-		if component == "linear":
-			path = file_path + "chi1.sm_xx_" + str(kpts) + "_" + str(ecut) + "-nospin_scissor_0_Nc_29"
-			epsilon = input_stage(path, "epsilon")
-			return epsilon
-		else:
-			path = file_path + "shgC.sm_" + component + "_" + str(kpts) + "_half-slab_" + str(ecut) + "-nospin_scissor_0_Nc_29"
-			chi = input_stage(path, "chi")
-			return chi
+	if component == "epsilon":
+		path = file_path + "chi1.sm_xx_" + str(kpts) + "_" + str(ecut) + "-nospin_scissor_0_Nc_29"
+		value = input_stage(path, "epsilon")
+	else:
+		path = file_path + "shgC.sm_" + component + "_" + str(kpts) + "_half-slab_" + str(ecut) + "-nospin_scissor_0_Nc_29"
+		value = input_stage(path, "chi")
+	return value
 
 def input_stage(file, type):
-	if type == "epsilon":
-		with open(file, 'rb') as csvfile:
-			data = csv.reader(csvfile, delimiter=' ', skipinitialspace=True)
+	with open(file, 'rb') as csvfile:
+		data = csv.reader(csvfile, delimiter=' ', skipinitialspace=True)
+		if type == "epsilon":
 			comp_list=[[row[0], row[2]] for row in data]
-			return comp_list
-	elif type == "chi":
-		with open(file, 'rb') as csvfile:
-			data = csv.reader(csvfile, delimiter=' ', skipinitialspace=True)
-			comp_list=[[row[0], row[3]] for row in data]
-			return comp_list
+		elif type == "chi":
+			comp_list=[[row[3]] for row in data]
+	return comp_list
 
-def rif_constants(frequency):
-	const = (32 * 3.14159265359 ** 3 * frequency ** 2) / (n_0 * 1.602176565e-19 ** 2 * 299792458.0 ** 3 * math.cos(theta) ** 2)
+def rif_constants(energy):
+	const = (32 * 3.14159265359 ** 3 * energy ** 2) / (n_0 * 1.602176565e-19 ** 2 * 299792458.0 ** 3 * math.cos(theta) ** 2)
 	return const
 
-def fresnel(polarization, material, frequency, linear): # REMOVE PLUS ONE FROM LINE 69 ASAP
+def fresnel(polarization, material, energy, epsilon): # REMOVE PLUS ONE FROM LINE 69 ASAP
 	if polarization == "s" and material == "vs":
-		t = (2 * math.cos(theta)) / (math.cos(theta) + wave_vector("s", frequency, linear))
+		t = (2 * math.cos(theta)) / (math.cos(theta) + wave_vector(energy, epsilon))
 	elif polarization == "s" and material == "sb":
-		t = (2 * wave_vector("s", frequency, linear)) / (1 + wave_vector("s", frequency, linear) + wave_vector("b", frequency, linear))
+		t = (2 * wave_vector(energy, epsilon)) / (1 + wave_vector(energy, epsilon) + wave_vector(energy, epsilon))
 	elif polarization == "p" and material == "vs":
-		t = (2 * math.cos(theta)) / (linear * math.cos(theta) + wave_vector("s", frequency, linear))
+		t = (2 * math.cos(theta)) / (epsilon * math.cos(theta) + wave_vector(energy, epsilon))
 	elif polarization == "p" and material == "sb":
-		t = (2 * wave_vector("s", frequency, linear)) / (linear * wave_vector("s", frequency, linear) + linear * wave_vector("b", frequency, linear))
+		t = (2 * wave_vector(energy, epsilon)) / (epsilon * wave_vector(energy, epsilon) + epsilon * wave_vector(energy, epsilon))
 	return t
 
-def r_factors(p_entry, p_exit, triperp, perpbipar, biparperp, tripar, frequency, linear):
-	if p_entry == "p" and p_exit == "p":
-		r = math.sin(theta) * linear * (((math.sin(theta) ** 2) * (linear ** 2) * triperp) + (wave_vector("b", frequency, linear) ** 2) * (linear ** 2) * perpbipar) + linear * linear * wave_vector("b", frequency, linear) * wave_vector("b", frequency, linear) * (-2 * math.sin(theta) * linear * biparperp + wave_vector("b", frequency, linear) * linear * tripar * math.cos(3 * phi))
-	elif p_entry == "s" and p_exit == "p":
-		r = math.sin(theta) * linear * perpbipar - wave_vector("b", frequency, linear) * linear * tripar * math.cos(3 * phi)
-	elif p_entry == "p" and p_exit == "s":
-		r = -(wave_vector("b", frequency, linear) ** 2) * (linear ** 2) * tripar * math.sin(3 * phi)
-	elif p_entry == "s" and p_exit == "s":
+def r_factors(polar_in, polar_out, triperp, perpbipar, biparperp, tripar, energy, epsilon):
+	if polar_in == "p" and polar_out == "p":
+		r = math.sin(theta) * epsilon * (((math.sin(theta) ** 2) * (epsilon ** 2) * triperp) + (wave_vector(energy, epsilon) ** 2) * (epsilon ** 2) * perpbipar) + epsilon * epsilon * wave_vector(energy, epsilon) * wave_vector(energy, epsilon) * (-2 * math.sin(theta) * epsilon * biparperp + wave_vector(energy, epsilon) * epsilon * tripar * math.cos(3 * phi))
+	elif polar_in == "s" and polar_out == "p":
+		r = math.sin(theta) * epsilon * perpbipar - wave_vector(energy, epsilon) * epsilon * tripar * math.cos(3 * phi)
+	elif polar_in == "p" and polar_out == "s":
+		r = -(wave_vector(energy, epsilon) ** 2) * (epsilon ** 2) * tripar * math.sin(3 * phi)
+	elif polar_in == "s" and polar_out == "s":
 		r = tripar * math.sin(3 * phi)
 	return r
 
-def wave_vector(layer, frequency, linear): # the absolute value thing is just temporary
-	if layer == "s":
-		k = (frequency / 299792458.0) * math.sqrt(math.fabs(linear - math.sin(theta) ** 2))
-	elif layer == "b":
-		k = (frequency / 299792458.0) * math.sqrt(math.fabs(linear - math.sin(theta) ** 2))
+def wave_vector(energy, epsilon): # the absolute value thing is just temporary
+	k = (energy / 299792458.0) * math.sqrt(math.fabs(epsilon - math.sin(theta) ** 2))
 	return k
 
-def output_stage(energies, outfile):
-	results = nonlinear_reflection_coefficient(entry, exit)
- 	with open(outfile, 'wb') as csvfile:
- 		output = csv.writer(csvfile)
- 		for row in results:
- 			output.writerow(row)
+def output_stage():
+	energy = [row[0] for row in values("epsilon")]
+	#print omega
+	print nonlinear_reflection_coefficient(entry, exit)
+	#out_file = file_path + "R" + entry + exit + "_" + str(kpts) + "_" + str(ecut)
+	#omega = [row[0] for row in values("epsilon")]
+	#results = nonlinear_reflection_coefficient(entry, exit)
+	#newdata = []
+	#with open(out_file, 'wb') as csvfile:
+ 	#	output = csv.writer(csvfile)
+	#	for c1, c2 in zip(omega, results):
+	#		c = "%s  %s" % (c1, c2)
+	#		newdata.append(c)
+	#	print newdata
+ 	#	#for row in new_data:
+ 	#	#	output.writerow(row)
 
-print nonlinear_reflection_coefficient(entry, exit)
+#print type(nonlinear_reflection_coefficient(entry, exit))
+output_stage()
+
