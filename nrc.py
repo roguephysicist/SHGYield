f70c4a51c6cb7447b37f6710c0b406b0aaf37b43
Sean Anderson <sean.martin.anderson@gmail.com>
2013-08-10 20:43:31 -0500

    program seems sound, needs corrected math

--- nrc.py
+++ nrc.py
@@ -1,20 +1,18 @@
 import math
 import random
-import csv
 from numpy import *
 from scipy import constants
-from itertools import izip
 
 ########### user input ###########
 #### XYZ file disorder
-disorder_path = "./sample_data/disorder/si_h_14.xyz" # full path to xyz file you want to disorder
+disorder_in = "./sample_data/disorder/si_h_14.xyz" # full path to xyz file you want to disorder
 l = 2.351 # bond length in angstrom for Si
 atoms = [2, 3, 4] # atoms you want to disorder
 disorder_amount = [0.1, 0.1, 0.1] # amount you want to disorder each atom
-repeat = 100 # number of times you want to repeat the disordering
+repeat = 1 # number of times you want to repeat the disordering
 
 #### Nonlinear reflection coefficient
-nrc_path = "./sample_data/res" # full path to 'res' folder for desired structure
+nrc_path = "./sample_data/res/" # full path to 'res' folder for desired structure
 kpts = 1219 # kpoints of the case you want to use
 ecut = 15 # ecut of the case you want to use
 # Polarization
@@ -32,14 +30,12 @@ theta = math.radians(theta_deg)
 phi = math.radians(phi_deg)
 
 ########### functions ###########
-#### formulas
 def disorder():
 	count = 1
 	while count <= repeat:
-		out_file = "./sample_data/disorder/si_h_14_mod_" + str(count).zfill(3) + ".xyz"
 		selected = [x - 1 for x in atoms]
 		bl_bohr = l * constants.angstrom / constants.value("Bohr radius")
-		xyz = load_matrix(disorder_path)
+		xyz = load_matrix("disorder")
 		data = xyz[selected]
 		new_xyz = xyz.copy()
 		final = zip(data, disorder_amount)
@@ -49,30 +45,27 @@ def disorder():
 			new_xyz[selected[atom], 0] = final[atom][0][0] + (final[atom][1] * (bl_bohr / 2) * math.sin(polar) * math.cos(azimuthal))
 			new_xyz[selected[atom], 1] = final[atom][0][1] + (final[atom][1] * (bl_bohr / 2) * math.sin(polar) * math.sin(azimuthal))
 			new_xyz[selected[atom], 2] = final[atom][0][2] + (final[atom][1] * (bl_bohr / 2) * math.cos(polar))
-		save_matrix(out_file, new_xyz)
+		out = "./sample_data/disorder/si_h_14_mod_" + str(count).zfill(3) + ".xyz"
+		save_matrix(out, new_xyz)
 		count += 1
 
-def nonlinear_reflection_coefficient(polar_in, polar_out):
-	energy = [row[0] for row in values("chi1")]
-	chi1 = [float(row[1]) for row in values("chi1")]
-	zzz = [float(row[1]) for row in values("zzz")]
-	zxx = [float(row[1]) for row in values("zxx")]
-	xxz = [float(row[1]) for row in values("xxz")]
-	xxx = [float(row[1]) for row in values("xxx")]
-	nrc = [[] for l in range(len(energy))]
-	i = 0
-	for energy, chi1, zzz, zxx, xxz, xxx in izip(energy, chi1, zzz, zxx, xxz, xxx):
-		R = rif_constants(float(energy)) * math.fabs(fresnel(polar_out, "vs", float(energy), chi1) * fresnel(polar_out, "sb", float(energy), chi1) * ((fresnel(polar_in, "vs", float(energy), chi1) * fresnel(polar_in, "sb", float(energy), chi1)) ** 2) * r_factors(polar_in, polar_out, zzz, zxx, xxz, xxx, float(energy), chi1)) ** 2
-		nrc[i].append(energy)
-		nrc[i].append(R)
-		i = i + 1
-	return nrc
+def nonlinear_reflection_coefficient():
+	e, chi1 = load_matrix("chi1")
+	twoe, chi1twoe = load_matrix("twoe")
+	zzz = load_matrix("zzz")
+	zxx = load_matrix("zxx")
+	xxz = load_matrix("xxz")
+	xxx = load_matrix("xxx")
+	R = rif_constants(e) * fabs(fresnel(exit, "vs", twoe, chi1twoe) * fresnel(exit, "sb", twoe, chi1twoe) * ((fresnel(entry, "vs", e, chi1) * fresnel(entry, "sb", e, chi1)) ** 2) * r_factors(entry, exit, zzz, zxx, xxz, xxx, e, twoe, chi1, chi1twoe)) ** 2
+	R = column_stack((e, R))
+	out = nrc_path + "R" + entry + exit + "_" + str(kpts) + "_" + str(ecut)
+	save_matrix(out, R)
 
 def rif_constants(energy):
-	const = (32 * constants.pi ** 3 * energy ** 2) / (n_0 * constants.e ** 2 * constants.c ** 3 * math.cos(theta) ** 2)
+	const = (32 * (constants.pi ** 3) * (energy ** 2)) / ((n_0 * constants.e ** 2) * (constants.c ** 3) * (math.cos(theta) ** 2))
 	return const
 
-def fresnel(polarization, material, energy, chi1): # Remove plus ones!!!
+def fresnel(polarization, material, energy, chi1):
 	if polarization == "s" and material == "vs":
 		t = (2 * math.cos(theta)) / (math.cos(theta) + wave_vector(energy, chi1))
 	elif polarization == "s" and material == "sb":
@@ -83,11 +76,11 @@ def fresnel(polarization, material, energy, chi1): # Remove plus ones!!!
 		t = (2 * wave_vector(energy, chi1)) / (epsilon(chi1) * wave_vector(energy, chi1) + epsilon(chi1) * wave_vector(energy, chi1))
 	return t
 
-def r_factors(polar_in, polar_out, triperp, perpbipar, biparperp, tripar, energy, chi1):
+def r_factors(polar_in, polar_out, triperp, perpbipar, biparperp, tripar, energy, twoenergy, chi1, chi1twoe):
 	if polar_in == "p" and polar_out == "p":
-		r = math.sin(theta) * epsilon(chi1) * (((math.sin(theta) ** 2) * (epsilon(chi1) ** 2) * triperp) + (wave_vector(energy, chi1) ** 2) * (epsilon(chi1) ** 2) * perpbipar) + epsilon(chi1) * epsilon(chi1) * wave_vector(energy, chi1) * wave_vector(energy, chi1) * (-2 * math.sin(theta) * epsilon(chi1) * biparperp + wave_vector(energy, chi1) * epsilon(chi1) * tripar * math.cos(3 * phi))
+		r = math.sin(theta) * epsilon(chi1twoe) * (((math.sin(theta) ** 2) * (epsilon(chi1) ** 2) * triperp) + (wave_vector(energy, chi1) ** 2) * (epsilon(chi1) ** 2) * perpbipar) + epsilon(chi1) * epsilon(chi1twoe) * wave_vector(energy, chi1) * wave_vector(twoenergy, chi1twoe) * (-2 * math.sin(theta) * epsilon(chi1) * biparperp + wave_vector(energy, chi1) * epsilon(chi1) * tripar * math.cos(3 * phi))
 	elif polar_in == "s" and polar_out == "p":
-		r = math.sin(theta) * epsilon(chi1) * perpbipar - wave_vector(energy, chi1) * epsilon(chi1) * tripar * math.cos(3 * phi)
+		r = math.sin(theta) * epsilon(chi1twoe) * perpbipar - wave_vector(twoenergy, chi1twoe) * epsilon(chi1twoe) * tripar * math.cos(3 * phi)
 	elif polar_in == "p" and polar_out == "s":
 		r = -(wave_vector(energy, chi1) ** 2) * (epsilon(chi1) ** 2) * tripar * math.sin(3 * phi)
 	elif polar_in == "s" and polar_out == "s":
@@ -98,38 +91,28 @@ def epsilon(chi1):
 	epsilon = 1 + (4 * constants.pi * chi1)
 	return epsilon
 
-def wave_vector(energy, chi1): # the absolute value thing is just temporary
-	k = (energy / constants.c) * math.sqrt(math.fabs(epsilon(chi1) - math.sin(theta) ** 2))
+def wave_vector(energy, chi1):
+	k = (energy / constants.c) * sqrt(epsilon(chi1) - (math.sin(theta) ** 2))
 	return k
 
-#### input/output
-def values(component):
-	if component == "chi1":
-		path = file_path + "chi1.sm_xx_yy_zz_" + str(kpts) + "_" + str(ecut) + "-nospin_scissor_0_Nc_29"
-		with open(path, 'rb') as csvfile:
-			data = csv.reader(csvfile, delimiter=' ', skipinitialspace=True)
-			value = [[row[0], row[1]] for row in data]
-	elif component == "zzz" or component == "zxx" or component == "xxz" or component == "xxx":
-		path = file_path + "shgC.sm_" + component + "_" + str(kpts) + "_half-slab_" + str(ecut) + "-nospin_scissor_0_Nc_29"
-		with open(path, 'rb') as csvfile:
-			data = csv.reader(csvfile, delimiter=' ', skipinitialspace=True)
-			value = [[row[0], row[3]] for row in data]
-	return value
-
-def output_stage():
-	out_file = file_path + "R" + entry + exit + "_" + str(kpts) + "_" + str(ecut)
-	with open(out_file, 'wb') as csvfile:
- 		output = csv.writer(csvfile, delimiter=' ')
- 		output.writerows(nonlinear_reflection_coefficient(entry, exit))
-
-def load_matrix(f):
-	data = loadtxt(f)
+def load_matrix(comp):
+	if comp == "disorder":
+		data = loadtxt(disorder_in)
+	elif comp == "chi1":
+		f = nrc_path + "chi1.sm_xx_yy_zz_" + str(kpts) + "_" + str(ecut) + "-nospin_scissor_0_Nc_29"
+		data = genfromtxt(f, skip_footer=1000, unpack=True, usecols=[0, 1])
+	elif comp == "twoe":
+		f = nrc_path + "chi1.sm_xx_yy_zz_" + str(kpts) + "_" + str(ecut) + "-nospin_scissor_0_Nc_29"
+		new = column_stack(loadtxt(f, unpack=True, usecols=[0, 2]))
+		wanted = array(2 * new[:,0]).reshape(-1).tolist()
+		data = column_stack(new[logical_or.reduce([new[:,0] == x for x in wanted])])
+	elif comp == "zzz" or comp == "zxx" or comp == "xxz" or comp == "xxx":
+		f = nrc_path + "shgC.sm_" + comp + "_" + str(kpts) + "_half-slab_" + str(ecut) + "-nospin_scissor_0_Nc_29"
+		data = genfromtxt(f, skip_footer=1000, unpack=True, usecols=[4])
 	return data
 
 def save_matrix(f, data):
-	savetxt(f, data, fmt=('%.14f'), delimiter='\t')
+	savetxt(f, data, fmt=('%5.14e'), delimiter='\t')
 
-disorder()
-#nonlinear_reflection_coefficient(entry, exit)
-
-
+#disorder()
+nonlinear_reflection_coefficient()
