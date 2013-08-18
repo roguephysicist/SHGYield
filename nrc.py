4690cb79e768bfd2c108f238b8e297bc682b508a
Sean Anderson <sean.martin.anderson@gmail.com>
2013-08-18 10:39:55 -0500

    functional but needs unit analysis. 10/10 pylint score.

--- nrc.py
+++ nrc.py
@@ -9,15 +9,16 @@ The work codified in this software can be found in Phys.Rev.B66, 195329(2002).
 
 from math import sin, cos, radians
 from scipy import constants, interpolate
-from numpy import loadtxt, savetxt, column_stack, absolute, sqrt, linspace
+from numpy import loadtxt, savetxt, column_stack, absolute,\
+                  sqrt, linspace, ones, complex128
 
 ########### user input ###########
-OUT_PATH = "./sample_data/nrc/"
-CHI1_PATH = "./sample_data/res/chi1.kk_xx_yy_zz_3107_25-nospin_scissor_0.5_Nc_8"
-ZZZ_PATH = "./sample_data/res/shgC.kk_zzz_145_half-slab_10-nospin_scissor_0.5_Nc_29"
-ZXX_PATH = "./sample_data/res/shgC.kk_zxx_145_half-slab_10-nospin_scissor_0.5_Nc_29"
-XXZ_PATH = "./sample_data/res/shgC.kk_xxz_145_half-slab_10-nospin_scissor_0.5_Nc_29"
-XXX_PATH = "./sample_data/res/shgC.kk_xxx_145_half-slab_10-nospin_scissor_0.5_Nc_29"
+OUT = "sample_data/nrc/"
+CHI1 = "sample_data/res/chi1.kk_xx_yy_zz_3107_25-nospin_scissor_0.5_Nc_8"
+ZZZ = "sample_data/res/shgC.kk_zzz_145_half-slab_10-nospin_scissor_0.5_Nc_29"
+ZXX = "sample_data/res/shgC.kk_zxx_145_half-slab_10-nospin_scissor_0.5_Nc_29"
+XXZ = "sample_data/res/shgC.kk_xxz_145_half-slab_10-nospin_scissor_0.5_Nc_29"
+XXX = "sample_data/res/shgC.kk_xxx_145_half-slab_10-nospin_scissor_0.5_Nc_29"
 # Angles
 THETA_DEG = 65 # angle of incidence in degrees
 PHI_DEG = 30 # azimuthal angle
@@ -28,12 +29,15 @@ ELEC_DENS = 1.5e10 # electronic density
 ## Housekeeping
 THETA_RAD = radians(THETA_DEG)
 PHI_RAD = radians(PHI_DEG)
+ENERGIES = linspace(0, 20, 2001) / \
+           constants.value("Planck constant over 2 pi in eV s")
 
 ########### functions ###########
 def nonlinear_reflection():
     """ calls the different math functions and returns matrix,
     which is written to file """
-    onee = linspace(0, 20, 2001)
+    onee = linspace(0, 20, 2001) / \
+           constants.value("Planck constant over 2 pi in eV s")
     twoe = 2 * onee
     polarization = [["p", "p"], ["p", "s"], ["s", "p"], ["s", "s"]]
     for state in polarization:
@@ -42,21 +46,19 @@ def nonlinear_reflection():
               fresnel_sb(state[0], onee)) ** 2) *
               reflection_components(state[0], state[1], onee, twoe)) ** 2
         nrc = column_stack((onee, nrc))
-        out = OUT_PATH + "R" + state[0] + state[1]
+        out = OUT + "R" + state[0] + state[1]
         save_matrix(out, nrc)
 
 def chi1_real(energy):
     """ creates spline from real part of chi1 matrix"""
-    energies = linspace(0, 20, 2001)
-    chi1 = load_matrix(CHI1_PATH)
-    interpolated = interpolate.InterpolatedUnivariateSpline(energies, chi1.real)
+    chi1 = load_matrix(CHI1)
+    interpolated = interpolate.InterpolatedUnivariateSpline(ENERGIES, chi1.real)
     return interpolated(energy)
 
 def chi1_imag(energy):
     """ creates spline from imaginary part of chi1 matrix"""
-    energies = linspace(0, 20, 2001)
-    chi1 = load_matrix(CHI1_PATH)
-    interpolated = interpolate.InterpolatedUnivariateSpline(energies, chi1.imag)
+    chi1 = load_matrix(CHI1)
+    interpolated = interpolate.InterpolatedUnivariateSpline(ENERGIES, chi1.imag)
     return interpolated(energy)
 
 def epsilon(energy):
@@ -88,22 +90,24 @@ def fresnel_vs(polarization, energy):
     return fresnel
 
 def fresnel_sb(polarization, energy):
-    """ math for fresnel factors from surface to bulk """
+    """ math for fresnel factors from surface to bulk. Fresnel model """
     if polarization == "s":
-        fresnel = (2 * wave_vector(energy)) / (wave_vector(energy)
-                   + wave_vector(energy))
+        fresnel = ones(2001, dtype=complex128)
+        #fresnel = (2 * wave_vector(energy)) / (wave_vector(energy)
+        #           + wave_vector(energy))
     elif polarization == "p":
-        fresnel = (2 * wave_vector(energy)) / (epsilon(energy) *
-        wave_vector(energy) + epsilon(energy) * wave_vector(energy))
+        fresnel = 1 / epsilon(energy)
+        #fresnel = (2 * wave_vector(energy)) / (epsilon(energy) *
+        #wave_vector(energy) + epsilon(energy) * wave_vector(energy))
     return fresnel
 
 def reflection_components(polar_in, polar_out, energy, twoenergy):
     """ math for different r factors. loads in different component matrices """
-    xxx = load_matrix(XXX_PATH)
+    xxx = load_matrix(XXX)
     if polar_in == "p" and polar_out == "p":
-        zzz = load_matrix(ZZZ_PATH)
-        zxx = load_matrix(ZXX_PATH)
-        xxz = load_matrix(XXZ_PATH)
+        zzz = load_matrix(ZZZ)
+        zxx = load_matrix(ZXX)
+        xxz = load_matrix(XXZ)
         r_factor = sin(THETA_RAD) * epsilon(twoenergy) * \
                 (((sin(THETA_RAD) ** 2) * (epsilon(energy) ** 2) * zzz) +
                 (wave_vector(energy) ** 2) * (epsilon(energy) ** 2) * zxx) \
@@ -113,7 +117,7 @@ def reflection_components(polar_in, polar_out, energy, twoenergy):
                 wave_vector(energy) * epsilon(energy) * xxx *
                 cos(3 * PHI_RAD))
     elif polar_in == "s" and polar_out == "p":
-        zxx = load_matrix(ZXX_PATH)
+        zxx = load_matrix(ZXX)
         r_factor = sin(THETA_RAD) * epsilon(twoenergy) * zxx - \
                wave_vector(twoenergy) * epsilon(twoenergy) * \
                xxx * cos(3 * PHI_RAD)
