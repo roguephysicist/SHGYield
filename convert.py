9b53784883afdcf4f4a1dc6b19bd6708e2d45472
roguephysicist <sean.martin.anderson@gmail.com>
2014-02-19 17:54:59 -0600

    beefed up convert.py

--- convert.py
+++ convert.py
@@ -1,13 +1,28 @@
-"""
-A quick and dirty script to convert from an xyz file from bohrs to angstroms
-"""
+""" A quick and dirty script to convert an xyz file to and from coordinate """
 
 import sys
 import os
+from scipy import constants
 from numpy import loadtxt, savetxt
 
-IN_FILE = sys.argv[1]
-OUT_FILE = os.path.splitext(IN_FILE)[0] + "_xangst.xyz"
-BOHR_COORDS = loadtxt(IN_FILE)
-ANGS_COORDS = BOHR_COORDS * 0.529177249
-savetxt(OUT_FILE, ANGS_COORDS, fmt=('%5.14f'))
+def conversion():
+    """ Does the actual conversion """
+    target = sys.argv[1]
+    in_file = sys.argv[2]
+    if target == "b":
+        factor = constants.angstrom / constants.value("Bohr radius")
+    elif target == "a":
+        factor = constants.value("Bohr radius") / constants.angstrom
+    else:
+        print "Fuck you! You done fucked up!"
+        exit(0)
+    source = loadtxt(in_file)
+    new = source * factor
+    save_coords(in_file, new)
+
+def save_coords(output, data):
+    """ Saves the new coordinates to a file """
+    out_file = os.path.splitext(output)[0] + "_mod.xyz"
+    savetxt(out_file, data, fmt=('%5.14f'), delimiter='\t')
+
+conversion()
