23d9d5b967d49c0304b5558076b42c61049f3ea3
Sean Anderson <sean.martin.anderson@gmail.com>
2014-09-14 21:58:27 -0500

    improved coordconv.py

--- python/coordconv.py
+++ python/coordconv.py
@@ -2,27 +2,27 @@
 
 import sys
 import os
+import numpy as np
 from scipy import constants
-from numpy import loadtxt, savetxt
 
 def conversion():
     """ Does the actual conversion """
-    target = sys.argv[1]
-    in_file = sys.argv[2]
-    if target == "b": # converts to bohrs
+    in_file = sys.argv[1]
+    target = sys.argv[2]
+    if target == "bohr": # converts to bohrs
         factor = constants.angstrom / constants.value("Bohr radius")
-    elif target == "a": # converts to angstroms
+    elif target == "angstrom": # converts to angstroms
         factor = constants.value("Bohr radius") / constants.angstrom
     else:
-        print "Fuck you! You done fucked up!"
+        print "Bad choice!"
         exit(0)
-    source = loadtxt(in_file)
+    source = np.loadtxt(in_file)
     new = source * factor
     save_coords(in_file, new)
 
 def save_coords(output, data):
     """ Saves the new coordinates to a file """
-    out_file = os.path.splitext(output)[0] + "_mod.xyz"
-    savetxt(out_file, data, fmt=('%5.14f'), delimiter='\t')
+    out_file = os.path.splitext(output)[0] + "_angstrom.xyz"
+    np.savetxt(out_file, data, fmt=('% 2.15e'), delimiter='        ')
 
 conversion()
