e9c0aabe8b4307fca54c6d4017ce56b87ef4cfe8
Sean Anderson <sean.martin.anderson@gmail.com>
2015-03-05 19:19:54 -0600

    lookin smooth you

--- nonrefco.py
+++ nonrefco.py
@@ -23,20 +23,158 @@ from numpy import loadtxt, savetxt, column_stack, absolute, \
 
 ########### User Input ###########
 
-CHI = sys.argv[1] # reads layers from command line
-XXX = sys.argv[2] # reads kpoints from command line
-XXZ = sys.argv[3] # reads ecut from command line
-ZXX = sys.argv[4] # reads N_c from command line
-ZZZ = sys.argv[5] # reads scissor correction from command line
-#OUT = "../calculated/nrc/"
+#CHI = sys.argv[1] # reads layers from command line
+#XXX = sys.argv[2] # reads kpoints from command line
+#XXZ = sys.argv[3] # reads ecut from command line
+#ZXX = sys.argv[4] # reads N_c from command line
+#ZZZ = sys.argv[5] # reads scissor correction from command line
+##OUT = "../calculated/nrc/"
 
 # Angles
 THETA_RAD = radians(65)
 PHI_RAD = radians(30)
 # Misc
-ELEC_DENS = 1e-28 # electronic density and scaling factor (1e-7 * 1e-21)
+ENERGY = linspace(0.01, 20, 2000) # for debugging only, don't need this motherfucking shit
 
 ########### Functions ###########
+
+### Equations 
+###
+def nonlinear_reflection(state, energy):
+    """
+    eq. 22: R_{if}
+    dependencies: polarization state arrays
+    parents: control
+    children: rif_constants, fresnel_vl, fresnel_lb, reflection_components
+    Calls math functions and returns numpy array for each polarization
+    """
+    onee = energy
+    twoe = 2 * onee
+    nrc = rif_constants(onee) * absolute(((fresnel_vl(state[1], twoe) *
+          fresnel_lb(state[1], twoe) * ((fresnel_vl(state[0], onee) *
+          fresnel_lb(state[0], onee)) ** 2)) / 2j) *
+          reflection_components(state[0], state[1], onee, twoe)) ** 2
+    return nrc
+
+def rif_constants(energy):
+    """
+    eq. 22: constant term outside absolute value
+    dependencies: 1w energy array
+    parents: nonlinear_reflection
+    children: none
+    Multiplies constants. "electrondensity" merits revision.
+    """
+    electrondensity = 1e-28 # electronic density and scaling factor (1e-7 * 1e-21)
+    const = (32 * (constants.pi ** 3) * ((energy / constants.value("Planck constant over 2 pi in eV s")) ** 2)) / (electrondensity * ((constants.c * 100) ** 3) * (cos(THETA_RAD) ** 2))
+    return const
+
+def fresnel_vl(polarization, energy):
+    """
+    eq. 26: t^{vl}_{s} and t^{vl}_{p}
+    dependencies: energy array
+    parents: nonlinear_reflection
+    children: epsilon, wave_vector
+    Calculates fresnel factors for vacuum to surface
+    """
+    if polarization == "s":
+        fresnel = (2 * cos(THETA_RAD)) / (cos(THETA_RAD) +
+                   wave_vector(energy))
+    elif polarization == "p":
+        fresnel = (2 * cos(THETA_RAD)) / (epsilon(energy) *
+                   cos(THETA_RAD) + wave_vector(energy))
+    return fresnel
+
+def fresnel_lb(polarization, energy):
+    """
+    eq. 27: t^{lb}_{s} and t^{lb}_{p}
+    dependencies: energy array
+    parents: nonlinear_reflection
+    children: epsilon, wave_vector
+    Calculates fresnel factors for surface to bulk
+    """
+    if polarization == "s":
+        fresnel = ones(2000, dtype=complex128)
+        # fresnel = (2 * wave_vector(energy)) / (wave_vector(energy)
+        #              + wave_vector(energy))
+    elif polarization == "p":
+        fresnel = 1 / epsilon(energy)
+        # fresnel = (2 * wave_vector(energy)) / (epsilon(energy)
+        #              * wave_vector(energy) + epsilon(energy)
+        #              * wave_vector(energy))
+    return fresnel
+
+def reflection_components(polar_in, polar_out, energy, twoenergy):
+    """
+    eqs. 34a--34d
+    dependencies: polarization states, 1w and 2w energy arrays
+    parents: nonlinear_reflection
+    children: load_shg, epsilon, wave_vector
+    Calculates r_{if} factors. Loads shg arrays and does a lot of matrix
+    operations. BMS program had electrostatic units multiplying shg arrays.
+    """
+    # zzz = electrostatic_units(energy) * load_shg(ZZZ)
+    # zxx = electrostatic_units(energy) * load_shg(ZXX)
+    # xxz = electrostatic_units(energy) * load_shg(XXZ)
+    # xxx = electrostatic_units(energy) * load_shg(XXX)
+    zzz = load_shg(ZZZ)
+    zxx = load_shg(ZXX)
+    xxz = load_shg(XXZ)
+    xxx = load_shg(XXX)
+    if polar_in == "p" and polar_out == "p":
+        r_factor = sin(THETA_RAD) * epsilon(twoenergy) * \
+                (((sin(THETA_RAD) ** 2) * (epsilon(energy) ** 2) * zzz) +
+                (wave_vector(energy) ** 2) * (epsilon(energy) ** 2) * zxx) \
+                 + epsilon(energy) * epsilon(twoenergy) * \
+                 wave_vector(energy) * wave_vector(twoenergy) * \
+                 (-2 * sin(THETA_RAD) * epsilon(energy) * xxz +
+                wave_vector(energy) * epsilon(energy) * xxx *
+                cos(3 * PHI_RAD))
+    elif polar_in == "s" and polar_out == "p":
+        r_factor = sin(THETA_RAD) * epsilon(twoenergy) * zxx - \
+               wave_vector(twoenergy) * epsilon(twoenergy) * \
+               xxx * cos(3 * PHI_RAD)
+    elif polar_in == "p" and polar_out == "s":
+        r_factor = -(wave_vector(energy) ** 2) * (epsilon(energy) ** 2) * \
+                     xxx * sin(3 * PHI_RAD)
+    elif polar_in == "s" and polar_out == "s":
+        r_factor = xxx * sin(3 * PHI_RAD)
+    return r_factor
+
+def epsilon(energy):
+    """
+    eq. ?? REFERENCE NEEDED
+    dependencies: energy array
+    parents: wave_vector, fresnel_vl, fresnel_lb, reflection_components
+    children: chi_spline
+    Combines splines for real and imaginary parts of Chi^(1)
+    """
+    chi1 = chi_spline("real", energy) + 1j * chi_spline("imag", energy)
+    linear = 4 * constants.pi * chi1
+    return linear
+
+def wave_vector(energy):
+    """
+    eq. 5: k_{z}(\omega)
+    dependencies: energy array
+    parents: fresnel_vl, fresnel_lb, reflection_components
+    children: epsilon
+    Calculates wave vector k
+    """
+    k = sqrt(epsilon(energy) - (sin(THETA_RAD) ** 2))
+    return k
+
+def electrostatic_units(energy):
+    """
+    coefficient to convert to appropriate electrostatic units
+    possibly deprecated?
+    """
+    area = (1 / ((2 * sqrt(2)) ** 2)) * 2 * sqrt(3)
+    factor = (1j * ((2 *
+              constants.value("Rydberg constant times hc in eV")) ** 5) *
+              1e-5 * 2.08e-15 *
+            ((constants.value("lattice parameter of silicon") / 1e-10) ** 3))\
+              / (area * ((energy) ** 3))
+    return factor
 
 ### Control Functions
 ###
@@ -104,144 +242,5 @@ def chi_spline(part, energy):
     interpolated = \
     interpolate.InterpolatedUnivariateSpline(energy, getattr(chi1, part))
     return interpolated(energy)
-
-### Equations 
-###
-def nonlinear_reflection(state, energy):
-    """
-    eq. 22: R_{if}
-    dependencies: polarization state arrays
-    parents: control
-    children: rif_constants, fresnel_vl, fresnel_lb, reflection_components
-    Calls math functions and returns numpy array for each polarization
-    """
-    onee = energy
-    twoe = 2 * onee
-    nrc = rif_constants(onee) * absolute(((fresnel_vl(state[1], twoe) *
-          fresnel_lb(state[1], twoe) * ((fresnel_vl(state[0], onee) *
-          fresnel_lb(state[0], onee)) ** 2)) / 2j) *
-          reflection_components(state[0], state[1], onee, twoe)) ** 2
-    return nrc
-
-def rif_constants(energy):
-    """
-    eq. 22: constant term outside absolute value
-    dependencies: 1w energy array
-    parents: nonlinear_reflection
-    children: none
-    Multiplies constants. "ELEC_DENS" merits revision.
-    """
-    const = (32 * (constants.pi ** 3) * (energy ** 2)) / (ELEC_DENS *
-              ((constants.c * 100) ** 3) * (cos(THETA_RAD) ** 2) *
-              (constants.value("Planck constant over 2 pi in eV s") ** 2))
-    return const
-
-def fresnel_vl(polarization, energy):
-    """
-    eq. 26: t^{vl}_{s} and t^{vl}_{p}
-    dependencies: energy array
-    parents: nonlinear_reflection
-    children: epsilon, wave_vector
-    Calculates fresnel factors for vacuum to surface
-    """
-    if polarization == "s":
-        fresnel = (2 * cos(THETA_RAD)) / (cos(THETA_RAD) +
-                   wave_vector(energy))
-    elif polarization == "p":
-        fresnel = (2 * cos(THETA_RAD)) / (epsilon(energy) *
-                   cos(THETA_RAD) + wave_vector(energy))
-    return fresnel
-
-def fresnel_lb(polarization, energy):
-    """
-    eq. 27: t^{lb}_{s} and t^{lb}_{p}
-    dependencies: energy array
-    parents: nonlinear_reflection
-    children: epsilon, wave_vector
-    Calculates fresnel factors for surface to bulk
-    """
-    if polarization == "s":
-        fresnel = ones(2000, dtype=complex128)
-        # fresnel = (2 * wave_vector(energy)) / (wave_vector(energy)
-        #              + wave_vector(energy))
-    elif polarization == "p":
-        fresnel = 1 / epsilon(energy)
-        # fresnel = (2 * wave_vector(energy)) / (epsilon(energy)
-        #              * wave_vector(energy) + epsilon(energy)
-        #              * wave_vector(energy))
-    return fresnel
-
-def reflection_components(polar_in, polar_out, energy, twoenergy):
-    """
-    eqs. 34a--34d
-    dependencies: polarization states, 1w and 2w energy arrays
-    parents: nonlinear_reflection
-    children: load_shg, epsilon, wave_vector
-    Calculates r_{if} factors. Loads shg arrays and does a lot of matrix
-    operations. BMS program had electrostatic units multiplying shg arrays.
-    """
-    zzz = load_shg(ZZZ)
-    zxx = load_shg(ZXX)
-    xxz = load_shg(XXZ)
-    xxx = load_shg(XXX)
-    # zzz = electrostatic_units(energy) * load_shg(ZZZ)
-    # zxx = electrostatic_units(energy) * load_shg(ZXX)
-    # xxz = electrostatic_units(energy) * load_shg(XXZ)
-    # xxx = electrostatic_units(energy) * load_shg(XXX)
-    if polar_in == "p" and polar_out == "p":
-        r_factor = sin(THETA_RAD) * epsilon(twoenergy) * \
-                (((sin(THETA_RAD) ** 2) * (epsilon(energy) ** 2) * zzz) +
-                (wave_vector(energy) ** 2) * (epsilon(energy) ** 2) * zxx) \
-                 + epsilon(energy) * epsilon(twoenergy) * \
-                 wave_vector(energy) * wave_vector(twoenergy) * \
-                 (-2 * sin(THETA_RAD) * epsilon(energy) * xxz +
-                wave_vector(energy) * epsilon(energy) * xxx *
-                cos(3 * PHI_RAD))
-    elif polar_in == "s" and polar_out == "p":
-        r_factor = sin(THETA_RAD) * epsilon(twoenergy) * zxx - \
-               wave_vector(twoenergy) * epsilon(twoenergy) * \
-               xxx * cos(3 * PHI_RAD)
-    elif polar_in == "p" and polar_out == "s":
-        r_factor = -(wave_vector(energy) ** 2) * (epsilon(energy) ** 2) * \
-                     xxx * sin(3 * PHI_RAD)
-    elif polar_in == "s" and polar_out == "s":
-        r_factor = xxx * sin(3 * PHI_RAD)
-    return r_factor
-
-def epsilon(energy):
-    """
-    eq. ?? REFERENCE NEEDED
-    dependencies: energy array
-    parents: wave_vector, fresnel_vl, fresnel_lb, reflection_components
-    children: chi_spline
-    Combines splines for real and imaginary parts of Chi^(1)
-    """
-    chi1 = chi_spline("real", energy) + 1j * chi_spline("imag", energy)
-    linear = 4 * constants.pi * chi1
-    return linear
-
-def wave_vector(energy):
-    """
-    eq. 5: k_{z}(\omega)
-    dependencies: energy array
-    parents: fresnel_vl, fresnel_lb, reflection_components
-    children: epsilon
-    Calculates wave vector k
-    """
-    k = sqrt(epsilon(energy) - (sin(THETA_RAD) ** 2))
-    return k
-
-def electrostatic_units(energy):
-    """
-    coefficient to convert to appropriate electrostatic units
-    possibly deprecated?
-    """
-    area = (1 / ((2 * sqrt(2)) ** 2)) * 2 * sqrt(3)
-    factor = (1j * ((2 *
-              constants.value("Rydberg constant times hc in eV")) ** 5) *
-              1e-5 * 2.08e-15 *
-            ((constants.value("lattice parameter of silicon") / 1e-10) ** 3))\
-              / (area * ((energy) ** 3))
-    return factor
 
 control()
