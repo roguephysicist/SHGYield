57a13f6f11806726cfafa7d00c1221aa0b84be86
Sean Anderson <sean.martin.anderson@gmail.com>
2015-12-26 09:40:09 -0600

    generic fresnel factors and corrected three layer

--- nonrefco.py
+++ nonrefco.py
@@ -68,12 +68,26 @@ def shgcomp(in_file):
     shg = comp[:MAXE]
     return shg
 
+def fresnel(pol, i, j, freq):
+    """
+    Generic Fresnel factors
+    """
+    ki = eval("k" + i + freq)
+    kj = eval("k" + j + freq)
+    epsi = eval("eps" + i + freq)
+    epsj = eval("eps" + j + freq)
+    if pol == "p":
+        factor = (2 * ki * np.sqrt(epsi * epsj)) / (ki * epsj + kj * epsi)
+    elif pol == "s":
+        factor = (2 * ki) / (ki + kj)
+    return factor
+
 # reads input file
 param = parse_input()
 
 # angles
-thetarad = math.radians(float(param['theta']))
-phirad = math.radians(float(param['phi']))
+thetain = math.radians(float(param['theta']))
+phi = math.radians(float(param['phi']))
 
 # constants, conversions, and prefactor
 onee = np.linspace(0.01, 10, MAXE) # 1w energy array
@@ -85,31 +99,35 @@ pm2tom2 = 1e-24 # pm^2 to m^2
 m2tocm2 = 1e4 # m^2 to cm^2
 tinibascale = 1e6 # for scaling chi in 1e6 (pm^2/V)
 scale = 1e20 # for R in 1e-20 (cm^2/W)
-prefactor = 1 / (2 * eps0 * hbar**2 * lspeed**3 * math.cos(thetarad)**2)
+prefactor = 1 / (2 * eps0 * hbar**2 * lspeed**3 * math.cos(thetain)**2)
 
 # loads chi1 and epsilons
 epsl = epsilon(param['chil'])
 epsb = epsilon(param['chib'])
-epsl1w = epsl[0][:MAXE]
-epsl2w = epsl[0][1::2]
+epsv1w = 1
+epsv2w = 1
+epsl1w = epsb[0][:MAXE]
+epsl2w = 1
 epsb1w = epsb[0][:MAXE]
 epsb2w = epsb[0][1::2]
 
 # wave vectors for 1w and 2w
-kzl1w = np.sqrt(epsl1w - (math.sin(thetarad) ** 2))
-kzl2w = np.sqrt(epsl2w - (math.sin(thetarad) ** 2))
-kzb1w = np.sqrt(epsb1w - (math.sin(thetarad) ** 2))
-kzb2w = np.sqrt(epsb2w - (math.sin(thetarad) ** 2))
+kv1w = np.sqrt(epsv1w - (math.sin(thetain) ** 2))
+kv2w = np.sqrt(epsv2w - (math.sin(thetain) ** 2))
+kb1w = np.sqrt(epsb1w - (math.sin(thetain) ** 2))
+kb2w = np.sqrt(epsb2w - (math.sin(thetain) ** 2))
+kl1w = np.sqrt(epsl1w - (math.sin(thetain) ** 2))
+kl2w = np.sqrt(epsl2w - (math.sin(thetain) ** 2))
 
-# fresnel factors for 1w and 2w, s and p polarizations
-tvls = (2 * math.cos(thetarad)) / (math.cos(thetarad) + kzl1w)
-tvlp = (2 * math.cos(thetarad)) / (epsl1w * math.cos(thetarad) + kzl1w)
-tlbs = (2 * kzl1w) / (kzl1w + kzb1w)
-tlbp = (2 * kzl1w) / (epsb1w * kzl1w + epsl1w * kzb1w)
-Tvls = (2 * math.cos(thetarad)) / (math.cos(thetarad) + kzl2w)
-Tvlp = (2 * math.cos(thetarad)) / (epsl2w * math.cos(thetarad) + kzl2w)
-Tlbs = (2 * kzl2w) / (kzl2w + kzb2w)
-Tlbp = (2 * kzl2w) / (epsb2w * kzl2w + epsl2w * kzb2w)
+## fresnel factors for 1w and 2w, s and p polarizations
+tvls = fresnel("s", "v", "b", "1w")
+tvlp = fresnel("p", "v", "b", "1w")
+tlbs = fresnel("s", "b", "b", "1w")
+tlbp = fresnel("p", "b", "b", "1w")
+Tvls = fresnel("s", "v", "v", "2w")
+Tvlp = fresnel("p", "v", "v", "2w")
+Tlbs = fresnel("s", "v", "b", "2w")
+Tlbp = fresnel("p", "v", "b", "2w")
 
 # loads chi2, converts to cm^2/V, and screens them with layer epsilon
 zzz = (tinibascale * pm2tom2 * shgcomp(param['zzz']))
@@ -118,27 +136,28 @@ xxz = (tinibascale * pm2tom2 * shgcomp(param['xxz']))
 xxx = (tinibascale * pm2tom2 * shgcomp(param['xxx']))
 
 # r factors for different input and output polarizations
-rpp = math.sin(thetarad) * epsb2w * \
-      (((math.sin(thetarad) ** 2) * (epsb1w ** 2) * zzz) + \
-      (kzb1w ** 2) * (epsl1w ** 2) * zxx) + epsl1w * epsl2w * \
-      kzb1w * kzb2w * (-2 * math.sin(thetarad) * epsb1w * xxz - \
-      kzb1w * epsl1w * xxx * math.cos(3 * phirad))
-rps = -(kzb1w ** 2) * (epsl1w ** 2) * xxx * math.sin(3 * phirad)
-rsp = math.sin(thetarad) * epsb2w * zxx - \
-      kzb2w * epsl2w * xxx * math.cos(3 * phirad)
-rss = xxx * math.sin(3 * phirad)
+rpp = epsb2w * (math.sin(thetain)/np.sqrt(epsl2w)) * \
+     (epsb1w**2 * (math.sin(thetain)/np.sqrt(epsl2w))**2 * zzz + \
+      epsl1w**2 * kb1w**2 * zxx) - epsl1w * epsl2w * kb1w * kb2w * \
+     (2 * epsb1w * (math.sin(thetain)/np.sqrt(epsl2w)) * xxz + \
+      epsl1w * kb1w * xxx * math.cos(3 * phi))
+rps = -(kb1w ** 2) * (epsl1w ** 2) * xxx * math.sin(3 * phi)
+rsp = (epsb2w * (math.sin(thetain)/np.sqrt(epsl2w)) * zxx) - \
+      (epsl2w * kb2w * xxx * math.cos(3 * phi))
+rss = xxx * math.sin(3 * phi)
 
 # fresnel factors multiplied out for ease of debugging
-fpp = Tvlp * Tlbp * (tvlp * tlbp)**2
-fps = Tvls * Tlbs * (tvlp * tlbp)**2
-fsp = Tvlp * Tlbp * (tvls * tlbs)**2
-fss = Tvls * Tlbs * (tvls * tlbs)**2
+gammapp = ((Tvlp * Tlbp)/(epsl2w * np.sqrt(epsb2w))) * \
+          ((tvlp * tlbp)/(epsl1w * np.sqrt(epsb1w)))**2
+gammaps = Tvls * Tlbs * ((tvlp * tlbp)/(epsl1w * np.sqrt(epsb1w)))**2
+gammasp = ((Tvlp * Tlbp)/(epsl2w * np.sqrt(epsb2w))) * (tvls * tlbs)**2
+gammass = Tvls * Tlbs * (tvls * tlbs)**2
 
 # R factors for different input and output polarizations (in cm^2/W)
-Rpp = scale * m2tocm2 * prefactor * (onee ** 2) * np.absolute(fpp * rpp)**2
-Rps = scale * m2tocm2 * prefactor * (onee ** 2) * np.absolute(fps * rps)**2
-Rsp = scale * m2tocm2 * prefactor * (onee ** 2) * np.absolute(fsp * rsp)**2
-Rss = scale * m2tocm2 * prefactor * (onee ** 2) * np.absolute(fss * rss)**2
+Rpp = scale * m2tocm2 * prefactor * (onee ** 2) * np.absolute(gammapp * rpp)**2
+Rps = scale * m2tocm2 * prefactor * (onee ** 2) * np.absolute(gammaps * rps)**2
+Rsp = scale * m2tocm2 * prefactor * (onee ** 2) * np.absolute(gammasp * rsp)**2
+Rss = scale * m2tocm2 * prefactor * (onee ** 2) * np.absolute(gammass * rss)**2
 
 # creates columns for 2w and R factors and writes to file
 nrc = np.column_stack((onee, Rpp, Rps, Rsp, Rss))
