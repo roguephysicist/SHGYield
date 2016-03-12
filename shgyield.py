edc100e83271d2e552b0d68512c993102be733a0
Sean Anderson <sean.martin.anderson@gmail.com>
2016-03-12 01:14:24 -0600

    improved functions and added some debugging

--- shgyield.py
+++ shgyield.py
@@ -21,17 +21,20 @@ import math
 import numpy as np
 from scipy import constants
 
-# max energy, assumes range from 0 to 20 with 2001 steps
-MAXE = 1000
-ONEE = np.linspace(0.01, float(MAXE)/100, MAXE) # 1w energy array
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider, Button, RadioButtons
 
+## debugging variables
 CHI1NORM = 1.2659296143 # Normalization yo
-SLABHEIGHT = 71.09425 # In Bohrs
-HEIGHT2 = SLABHEIGHT*0.5 # Also in Bohrs biatch
+#SLAB = (142.1885172213904)*0.5 # In Bohrs
+#SLAB = 3.7621659771810236 # In nanometers
+SLAB = 100
+HEIGHT2 = 30 # Also in nanometers biatch
+##
 
-# parses input file specified in command line input
-def parse_input():
-    infile = sys.argv[1]
+
+#### Functions ####
+def parse_input(infile): # parses input file from command line
     params = {}
     targetfile = open(infile)
     data = targetfile.readlines()
@@ -44,54 +47,66 @@ def parse_input():
     targetfile.close()
     return params
 
-# loads chi1 file, unpacks columns, and creates numpy array
-def epsilon(in_file, norm): 
+def savefile(file, freq, val1, val2, val3, val4): # saves to file with header
+    data = np.column_stack((freq, val1, val2, val3, val4))
+    np.savetxt(file, data,
+           fmt=('%05.2f', '%.14e', '%.14e', '%.14e', '%.14e'),
+           delimiter='    ', header='RiF 1e-20 (cm^2/W)\n1w RpP RpS RsP RsS')
+
+def epsilon(in_file, norm): # loads chi1 file, creates epsilon numpy array
     data = np.loadtxt(in_file, unpack=True, skiprows=1)
-    real = (data[1] + data[3] + data[5])/3 # real average
-    imag = (data[2] + data[4] + data[6])/3 # imag average
-    coma = real + 1j * imag # complex average
-    epsa = 1 + (4 * constants.pi * coma * norm) # epsilon average
+    real = (data[1] + data[3] + data[5])/3      # real average
+    imag = (data[2] + data[4] + data[6])/3      # imag average
+    coma = real + 1j * imag                     # complex average
+    epsa = 1 + (4 * constants.pi * coma * norm) # epsilon with normalization
     return epsa
 
-# loads chi2 file, unpacks columns, sums columns, and creates numpy array
-def shgcomp(in_file):
+def shgcomp(in_file): # loads chi2 file, sums Re and Im, creates numpy array
     data = np.loadtxt(in_file, unpack=True, skiprows=1)
     comp = (data[1] + data[3]) + 1j * (data[2] + data[4])
     shg = comp[:MAXE]
     return shg
 
-# most generic fresnel factors
-def fresnel(pol, i, j, freq):
-    ki = eval("w" + i + freq)
-    kj = eval("w" + j + freq)
+def fresnel(pol, i, j, freq): # generic fresnel factors (see Mizrahi and Sipe)
+    wi = eval("w" + i + freq)
+    wj = eval("w" + j + freq)
     epsi = eval("eps" + i + freq)
     epsj = eval("eps" + j + freq)
     if pol == "p":
-        factor = (2 * ki * np.sqrt(epsi * epsj)) / \
-                 (ki * epsj + kj * epsi)
+        factor = (2 * wi * np.sqrt(epsi * epsj)) / \
+                 (wi * epsj + wj * epsi)
     elif pol == "s":
-        factor = (2 * ki) / (ki + kj)
+        factor = (2 * wi) / (wi + wj)
     return factor
 
-# reads input file and establishes mode
-PARAM = parse_input()
-MODE = str(PARAM['mode'])
 
-# angles
-THETAIN = math.radians(float(PARAM['theta']))
-PHI = math.radians(float(PARAM['phi']))
+#### Init ####
+PARAM = parse_input(sys.argv[1])    # parses input file
+MODE = str(PARAM['mode'])           # establishes mode
 
-# constants, conversions, and prefactor
+
+#### Energy ####
+## assumes range from 0 to 20 with 2001 steps
+MAXE = 1000
+ONEE = np.linspace(0.01, float(MAXE)/100, MAXE) # 1w energy array
+
+
+#### Constants ####
 HBAR = constants.value("Planck constant over 2 pi in eV s")
-EPS0 = constants.epsilon_0 # (F/m)
-LSPEED = constants.c # (m/s)
-N0E = 1.11e19 # for silicon, from PRB 81, 3781 Ref. 17 (V/m^2)
-PM2TOM2 = 1e-24 # pm^2 to m^2
-M2TOCM2 = 1e4 # m^2 to cm^2
-TINIBASCALE = 1e6 # for scaling chi in 1e6 (pm^2/V)
-SCALE = 1e20 # for R in 1e-20 (cm^2/W)
-PREFACTOR = 1 / (2 * EPS0 * HBAR**2 * LSPEED**3 * math.cos(THETAIN)**2)
+PLANCK = constants.value("Planck constant in eV s")
+EPS0 = constants.epsilon_0              # (F/m)
+LSPEED = constants.c                    # (m/s)
+N0E = 1.11e19                           # for Si, PRB81.3781 (Ref. 17) (V/m^2)
+PM2TOM2 = 1e-24                         # pm^2 to m^2
+M2TOCM2 = 1e4                           # m^2 to cm^2
+TINIBASCALE = 1e6                       # for scaling chi2 in 1e6 (pm^2/V)
+SCALE = 1e20                            # for R in 1e-20 (cm^2/W)
+THETA0 = math.radians(float(PARAM['theta'])) # converts theta to radians
+PHI = math.radians(float(PARAM['phi'])) # converts phi to radians
+PREFACTOR = 1 / (2 * EPS0 * HBAR**2 * LSPEED**3 * math.cos(THETA0)**2)
 
+
+#### Math ####
 # loads chi2, converts to m^2/V
 ZZZ = (TINIBASCALE * PM2TOM2 * shgcomp(PARAM['zzz']))
 ZXX = (TINIBASCALE * PM2TOM2 * shgcomp(PARAM['zxx']))
@@ -136,12 +151,12 @@ nl = np.sqrt(epsl1w)
 Nl = np.sqrt(epsl2w)
 
 # wave vectors for 1w and 2w
-wv1w = np.sqrt(epsv1w - (math.sin(THETAIN) ** 2))
-wv2w = np.sqrt(epsv2w - (math.sin(THETAIN) ** 2))
-wb1w = np.sqrt(epsb1w - (math.sin(THETAIN) ** 2))
-wb2w = np.sqrt(epsb2w - (math.sin(THETAIN) ** 2))
-wl1w = np.sqrt(epsl1w - (math.sin(THETAIN) ** 2))
-wl2w = np.sqrt(epsl2w - (math.sin(THETAIN) ** 2))
+wv1w = np.sqrt(epsv1w - (math.sin(THETA0) ** 2))
+wv2w = np.sqrt(epsv2w - (math.sin(THETA0) ** 2))
+wb1w = np.sqrt(epsb1w - (math.sin(THETA0) ** 2))
+wb2w = np.sqrt(epsb2w - (math.sin(THETA0) ** 2))
+wl1w = np.sqrt(epsl1w - (math.sin(THETA0) ** 2))
+wl2w = np.sqrt(epsl2w - (math.sin(THETA0) ** 2))
 
 ## fresnel factors for 1w and 2w, s and p polarizations
 tvls = fresnel("s", "v", ell1w, "1w")
@@ -152,48 +167,67 @@ Tvls = fresnel("s", "v", ell2w, "2w")
 Tvlp = fresnel("p", "v", ell2w, "2w")
 Tlbs = fresnel("s", ell2w, "b", "2w")
 Tlbp = fresnel("p", ell2w, "b", "2w")
-
-
-#### multiple reflections framework
-delta0 = 8 * math.pi * \
-((ONEE * HEIGHT2 * constants.value("Bohr radius"))/(HBAR * constants.c)) * \
-np.sqrt(Nl**2 - math.sin(THETAIN)**2)
-
-delta = 8 * math.pi * \
-((ONEE * SLABHEIGHT * constants.value("Bohr radius"))/(HBAR * constants.c)) *\
-np.sqrt(Nl**2 - math.sin(THETAIN)**2)
-
-edelta0 = np.exp(1j * delta0)
-edelta = np.exp(1j * delta)
-
 Rvlp = ((Nl/Nv) * Tvlp) - 1
 Rlbp = ((Nb/Nl) * Tlbp) - 1
 Rvls = Tvls - 1
 Rlbs = Tlbs - 1
 
+#### multiple reflections framework
+lambda0 = (PLANCK * LSPEED * 1e9)/ONEE # In nanometers
+
+delta0 = 8 * math.pi * ((ONEE * HEIGHT2 * 1e-9)/(PLANCK * LSPEED)) * \
+                        np.sqrt(Nl**2 - math.sin(THETA0)**2)
+delta = 8 * math.pi * ((ONEE * SLAB * 1e-9)/(PLANCK * LSPEED)) *\
+                        np.sqrt(Nl**2 - math.sin(THETA0)**2)
+
+edelta0 = np.exp(1j * delta0)
+edelta = np.exp(1j * delta)
+
 RMp = (Rlbp * edelta0)/(1 + (Rvlp * Rlbp * edelta))
 RMs = (Rlbs * edelta0)/(1 + (Rvls * Rlbs * edelta))
-
 RMpplus = 1 + RMp
 RMsplus = 1 + RMs
 RMpminus = 1 - RMp
 RMsminus = 1 - RMs
+####
 
-rMRpP = RMpplus * (math.sin(THETAIN)) * \
-          ((nb**4 * math.sin(THETAIN)**2 * ZZZ) + (nl**4 * wb1w**2 * ZXX)) \
-    - RMpminus * nl**2 * wb1w * wl2w * \
-        ((2 * nb**2 * math.sin(THETAIN) * XXZ) 
+# r factors for different input and output polarizations
+## with multiple reflections
+rMRpP = RMpplus * (math.sin(THETA0)) * \
+          ((nb**4 * math.sin(THETA0)**2 * ZZZ) + (nl**4 * wb1w**2 * ZXX)) \
+      - RMpminus * nl**2 * wb1w * wl2w * \
+        ((2 * nb**2 * math.sin(THETA0) * XXZ) 
                 + (nl**2 * wb1w * XXX * math.cos(3 * PHI)))
 rMRpS = -nl**4 * wb1w**2 * XXX * math.sin(3 * PHI)
-rMRsP = (RMpplus * math.sin(THETAIN) * ZXX) + \
+rMRsP = (RMpplus * math.sin(THETA0) * ZXX) + \
       (RMpminus * wl2w * XXX * math.cos(3 * PHI))
 rMRsS = XXX * math.sin(3 * PHI)
+## without multiple reflections
+rpP = Nb**2 * (math.sin(THETA0)) * \
+     ((nb**4 * math.sin(THETA0)**2 * ZZZ) + (nl**4 * wb1w**2 * ZXX)) \
+    - nl**2 * Nl**2 * wb1w * wb2w * \
+       ((2 * nb**2 * math.sin(THETA0) * XXZ)
+         + (nl**2 * wb1w * XXX * math.cos(3 * PHI)))
+rpS = - nl**4 * wb1w**2 * XXX * math.sin(3 * PHI)
+rsP = (Nb**2 * math.sin(THETA0) * ZXX)\
+        + (Nl**2 * wb2w * XXX * math.cos(3 * PHI))
+rsS = XXX * math.sin(3 * PHI)
 
+# fresnel factors multiplied out for ease of debugging
+## with multiple reflections
 GammaMRpP = (Tvlp/Nl) * ((tvlp * tlbp)/(nl**2 * nb))**2
 GammaMRpS = Tvls * RMsplus * ((tvlp * tlbp)/(nl**2 * nb))**2
 GammaMRsP = (Tvlp/Nl) * (tvls * tlbs)**2
 GammaMRsS = Tvls * Tlbs * (tvls * tlbs)**2
+## without multiple reflections
+GammapP = ((Tvlp * Tlbp)/(Nl**2 * Nb)) * ((tvlp * tlbp)/(nl**2 * nb))**2
+GammapS = Tvls * Tlbs * ((tvlp * tlbp)/(nl**2 * nb))**2
+GammasP = ((Tvlp * Tlbp)/(Nl**2 * Nb)) * (tvls * tlbs)**2
+GammasS = Tvls * Tlbs * (tvls * tlbs)**2
 
+# R factors for different input and output polarizations (in cm^2/W)
+#### CONSIDER FUNCTIONALIZING THIS
+## with multiple reflections
 RMRpP = SCALE * M2TOCM2 * PREFACTOR * (ONEE ** 2) * \
       np.absolute((1/nl) * GammaMRpP * rMRpP)**2
 RMRpS = SCALE * M2TOCM2 * PREFACTOR * (ONEE ** 2) * \
@@ -202,27 +236,7 @@ RMRsP = SCALE * M2TOCM2 * PREFACTOR * (ONEE ** 2) * \
       np.absolute((1/nl) * GammaMRsP * rMRsP)**2
 RMRsS = SCALE * M2TOCM2 * PREFACTOR * (ONEE ** 2) * \
       np.absolute((1/nl) * GammaMRsS * rMRsS)**2
-####
-
-
-# r factors for different input and output polarizations
-rpP = Nb**2 * (math.sin(THETAIN)) * \
-     ((nb**4 * math.sin(THETAIN)**2 * ZZZ) + (nl**4 * wb1w**2 * ZXX)) \
-    - nl**2 * Nl**2 * wb1w * wb2w * \
-       ((2 * nb**2 * math.sin(THETAIN) * XXZ)
-         + (nl**2 * wb1w * XXX * math.cos(3 * PHI)))
-rpS = - nl**4 * wb1w**2 * XXX * math.sin(3 * PHI)
-rsP = (Nb**2 * math.sin(THETAIN) * ZXX)\
-        + (Nl**2 * wb2w * XXX * math.cos(3 * PHI))
-rsS = XXX * math.sin(3 * PHI)
-
-# fresnel factors multiplied out for ease of debugging
-GammapP = ((Tvlp * Tlbp)/(Nl**2 * Nb)) * ((tvlp * tlbp)/(nl**2 * nb))**2
-GammapS = Tvls * Tlbs * ((tvlp * tlbp)/(nl**2 * nb))**2
-GammasP = ((Tvlp * Tlbp)/(Nl**2 * Nb)) * (tvls * tlbs)**2
-GammasS = Tvls * Tlbs * (tvls * tlbs)**2
-
-# R factors for different input and output polarizations (in cm^2/W)
+## without multiple reflections
 RpP = SCALE * M2TOCM2 * PREFACTOR * (ONEE ** 2) * \
       np.absolute((1/nl) * GammapP * rpP)**2
 RpS = SCALE * M2TOCM2 * PREFACTOR * (ONEE ** 2) * \
@@ -232,14 +246,9 @@ RsP = SCALE * M2TOCM2 * PREFACTOR * (ONEE ** 2) * \
 RsS = SCALE * M2TOCM2 * PREFACTOR * (ONEE ** 2) * \
       np.absolute((1/nl) * GammasS * rsS)**2
 
-# creates columns for 2w and R factors and writes to file
-NRC = np.column_stack((ONEE, RpP, RpS, RsP, RsS))
-NRCMR = np.column_stack((ONEE, RMRpP, RMRpS, RMRsP, RMRsS))
-OUTF = PARAM['output']
-OUTFMR = PARAM['outputMR']
-np.savetxt(OUTF, NRC,
-           fmt=('%05.2f', '%.14e', '%.14e', '%.14e', '%.14e'),
-           delimiter='    ', header='RiF 1e-20 (cm^2/W)\n1w RpP RpS RsP RsS')
-np.savetxt(OUTFMR, NRCMR,
-           fmt=('%05.2f', '%.14e', '%.14e', '%.14e', '%.14e'),
-           delimiter='    ', header='RiF 1e-20 (cm^2/W)\n1w RpP RpS RsP RsS')
+test1 = np.absolute(Rlbp)
+test2 = np.absolute(Rlbp/(1 + Rvlp*Rlbp))
+#### Output ####
+#savefile(PARAM['output'], ONEE, RpP, RpS, RsP, RsS)
+#savefile(PARAM['outputMR'], ONEE, RMRpP, RMRpS, RMRsP, RMRsS)
+savefile("test", ONEE, test1.real, test1.imag, test2.real, test2.imag)
